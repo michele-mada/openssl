c1e775e5dc81e9f8f85dd53cfa6fe79d84c9621e
michele-mada <michele.mm1234@gmail.com>
2018-01-26 18:25:58 +0100

    test mem improvement 2

--- apps/enc.c
+++ apps/enc.c
@@ -666,8 +666,7 @@ int enc_main(int argc, char **argv)
 
 static void *enc_write_worker(void *param) {
     struct enc_worker_param *p = (struct enc_worker_param*) param;
-    char *data = aligned_alloc(64, p->inl);
-    BIO *mem = BIO_new_mem_buf(data, 0);
+    BIO *mem = BIO_new(BIO_s_mem());
     BIO *benc = BIO_dup_chain(p->first_stage_model);
     BIO *first_stage = BIO_push(benc, mem);
     
